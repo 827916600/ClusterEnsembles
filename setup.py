b4c171b06b0e8213c6d74b5635a91e17fd44aaf4
Takehiro Sano <sano@momo.cs.okayama-u.ac.jp>
2021-02-02 18:29:09 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='ClusterEnsembles',  
+    version='0.1.0',  
+    packages=find_packages(),  
+
+    author='Takehiro Sano',  
+    author_email='tsano430@gmail.com',  
+
+    url='https://github.com/tsano430/ClusterEnsembles',  
+
+    description='A Python package for cluster ensembles', 
+    long_description=open('README.md').read(), 
+    long_description_content_type='text/markdown',  
+
+    python_requires='>=3.6, <4',
+    
+    classifiers=[  
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Operating System :: OS Independent',
+    ],
+
+    install_requires=[
+        'numpy',
+        'networkx',
+        'pymetis',
+        'scikit-learn', 
+    ]
+)
