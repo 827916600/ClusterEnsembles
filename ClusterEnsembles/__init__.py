3cae98c8899a49aa540b8198d13f53453bef2b83
Takehiro Sano <sano@momo.cs.okayama-u.ac.jp>
2021-02-02 17:13:12 +0900

    Add cluster ensembles

---
+++ ClusterEnsembles/__init__.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+
+from .ClusterEnsembles import cluster_ensembles
+
+__all__ = ['cluster_ensembles']
+
---
+++ ClusterEnsembles/tests/test_hbgf.py
@@ -0,0 +1,24 @@
+# test_hbgf.py
+
+
+import unittest
+import numpy as np
+from sklearn.metrics import normalized_mutual_info_score
+import ClusterEnsembles as CE
+
+
+class AlgsTest(unittest.TestCase):
+
+    def test_hbgf(self):
+        base_clusters = np.array([
+            [1, 1, 1, 2, 2, 3, 3],
+            [2, 2, 2, 3, 3, 1, 1],
+            [1, 1, 2, 2, 3, 3, 3],
+            [1, 2, np.nan, 1, 2, np.nan, np.nan]
+        ])
+        label_true = np.array([1, 1, 1, 2, 2, 3, 3])
+        label_pred = CE.cluster_ensembles(base_clusters, solver='hbgf')
+        nmi_score = normalized_mutual_info_score(label_true, label_pred, average_method='geometric')
+        self.assertEqual(1.0, nmi_score)
+
+
