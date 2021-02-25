65fffa80d555b2acd6c3b7417bb8106ef5a7792b
Takehiro Sano <tsano430@gmail.com>
2021-02-25 16:40:08 +0900

    Add tests (#11)

---
+++ ClusterEnsembles/tests/data/create_test_data.py
@@ -0,0 +1,28 @@
+# create_test_data.py
+
+import numpy as np
+from sklearn.cluster import KMeans
+
+np.random.seed(0)
+
+locs = [-10, -5, 0, 5, 10]
+for i, loc in enumerate(locs):
+    normal = np.random.normal(loc=loc, scale=1.0, size=1600).reshape(200, 8)
+    if i == 0:
+        normals = normal
+    else:
+        normals = np.vstack((normals, normal))
+normals = normals[np.random.permutation(np.arange(normals.shape[0])), :]
+
+kmeans = KMeans(n_clusters=5)
+bcs = []
+for i in range(8//2):
+    bc = kmeans.fit(normals[:, 2*i:2*(i+1)]).labels_
+    bcs.append(bc)
+base_clusters = np.array(bcs)
+
+label_true = base_clusters[0]
+base_clusters = base_clusters[1:]
+
+np.savetxt('label_true.csv', label_true.reshape(1, len(label_true)), delimiter=',', fmt='%d')
+np.savetxt('base_clusters.csv', base_clusters, delimiter=',', fmt='%d')
--- ClusterEnsembles/tests/test_cspa.py
+++ ClusterEnsembles/tests/test_cspa.py
@@ -1,6 +1,7 @@
 # test_cspa.py
 
 
+import os
 import unittest
 import numpy as np
 from sklearn.metrics import normalized_mutual_info_score
@@ -9,7 +10,7 @@ import ClusterEnsembles as CE
 
 class CSPATest(unittest.TestCase):
 
-    def test_cspa(self):
+    def test_missing(self):
         base_clusters = np.array([
             [1, 1, 1, 2, 2, 3, 3],
             [2, 2, 2, 3, 3, 1, 1],
@@ -20,3 +21,11 @@ class CSPATest(unittest.TestCase):
         label_pred = CE.cluster_ensembles(base_clusters, solver='cspa')
         nmi_score = normalized_mutual_info_score(label_true, label_pred, average_method='geometric')
         self.assertEqual(1.0, nmi_score)
+
+    def test_performance(self):
+        base_clusters = np.loadtxt(os.path.dirname(__file__) + '/data/base_clusters.csv', delimiter=',', dtype=int)
+        label_true = np.loadtxt(os.path.dirname(__file__) + '/data/label_true.csv', delimiter=',', dtype=int)
+        label_pred = CE.cluster_ensembles(base_clusters, solver='cspa')
+        nmi_score = normalized_mutual_info_score(label_true, label_pred, average_method='geometric')
+        self.assertEqual(1.0, nmi_score)
+
