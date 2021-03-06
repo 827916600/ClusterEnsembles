85b276581f50fb6183a238702759cf6472da1018
Takehiro Sano <tsano430@gmail.com>
2021-03-06 14:24:41 +0900

    Add random_state for reproducible results (#18)
    * Add random_state
    
    * Update README.md

--- ClusterEnsembles/tests/test_mcla.py
+++ ClusterEnsembles/tests/test_mcla.py
@@ -18,14 +18,14 @@ class MCLATest(unittest.TestCase):
             [1, 2, np.nan, 1, 2, np.nan, np.nan]
         ])
         label_true = np.array([1, 1, 1, 2, 2, 3, 3])
-        label_pred = CE.cluster_ensembles(base_clusters, solver='mcla')
+        label_pred = CE.cluster_ensembles(base_clusters, solver='mcla', random_state=0)
         nmi_score = normalized_mutual_info_score(label_true, label_pred, average_method='geometric')
         self.assertEqual(1.0, nmi_score)
 
     def test_performance(self):
         base_clusters = np.loadtxt(os.path.dirname(__file__) + '/data/base_clusters.csv', delimiter=',', dtype=int)
         label_true = np.loadtxt(os.path.dirname(__file__) + '/data/label_true.csv', delimiter=',', dtype=int)
-        label_pred = CE.cluster_ensembles(base_clusters, solver='mcla')
+        label_pred = CE.cluster_ensembles(base_clusters, solver='mcla', random_state=0)
         nmi_score = normalized_mutual_info_score(label_true, label_pred, average_method='geometric')
         self.assertEqual(1.0, nmi_score)
 
--- ClusterEnsembles/tests/test_utils.py
+++ ClusterEnsembles/tests/test_utils.py
@@ -11,7 +11,6 @@ import ClusterEnsembles as CE
 class UtilsTest(unittest.TestCase):
 
     def test_all(self):
-        np.random.seed(0)
         base_clusters = np.array([
             [1, 1, 1, 2, 2, 3, 3],
             [2, 2, 2, 3, 3, 1, 1],
@@ -19,7 +18,7 @@ class UtilsTest(unittest.TestCase):
             [1, 2, np.nan, 1, 2, np.nan, np.nan]
         ])
         label_true = np.array([1, 1, 1, 2, 2, 3, 3])
-        label_pred = CE.cluster_ensembles(base_clusters, solver='all')
+        label_pred = CE.cluster_ensembles(base_clusters, solver='all', random_state=0)
         nmi_score = normalized_mutual_info_score(label_true, label_pred, average_method='geometric')
         self.assertEqual(1.0, nmi_score)
 
